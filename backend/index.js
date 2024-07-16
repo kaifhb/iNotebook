8d749d46619c70598d681c17f7c7288e37e546a6
kaifhb <97424696+kaifhb@users.noreply.github.com>
2024-07-17 01:55:01 +0530

    vercel done

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,30 @@
 const connectToMongo = require("./db");
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
 require("dotenv").config();
 
 connectToMongo();
 const app = express();
 app.use(cors());
-const port = 5000
+const port = 5000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('kaif.....')
-})
-
 // Available routes
-app.use('/api/auth',require('./routes/auth'));
-app.use('/api/notes',require('./routes/notes'));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
+
+app.use(express.static(path.resolve(__dirname, "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve("build", "index.html")); // Corrected method here
+});
+
+app.get("/", (req, res) => {
+  res.send("kaif.....");
+});
 
 app.listen(port, () => {
-  console.log(`iNotebook app listening on port ${port}`)
-})
+  console.log(`iNotebook app listening on port ${port}`);
+});
